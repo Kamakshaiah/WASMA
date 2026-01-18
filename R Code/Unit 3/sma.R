9bcfb03d50309f377f0d887ec1675588e302c861
Dr. M. Kamakshaiah <37700934+Kamakshaiah@users.noreply.github.com>
2026-01-18 21:40:33 +0530

    Add files via upload

--- R Code/Unit 3/sma.R
+++ R Code/Unit 3/sma.R
@@ -1,10 +1,46 @@
+## 1. engagement
 # likes <- c(120, 340, 560)
 # comments <- c(30, 45, 80)
 # shares <- c(20, 60, 90)
 # followers <- c(1000, 2500, 4000)
-# 
+
 # engagement_rate <- (likes + comments + shares) / followers
 # engagement_rate
+
+## 2. sentiment
+
+# library(tibble)
+# sentiment_data <- tibble(
+#   sentiment = c('positive','negative','positive','neutral','negative')
+# )
+
+## Sentiment Calculation (R):
+# sort(table(sentiment_data), decreasing = TRUE)
+
+# 3. Influencer Score 
+# library(dplyr)
+# influencer_data <- tibble(
+#   influencer = c('A','B','C'),
+#   followers = c(10000, 25000, 40000),
+#   likes = c(500, 800, 1200),
+#   comments = c(50, 120, 300)
+# )
+# 
+# influencer_data %>%
+#   mutate(engagement_rate = (likes + comments)/followers,
+#          influencer_score = followers * engagement_rate)
+
+
+# 4. Hashtag Growth Rate 
+
+# hashtag_data <- tibble(
+#   date = as.Date(c('2025-01-01','2025-01-02','2025-01-03')),
+#   hashtag = c('#AI','#AI','#AI')
+# )
+# 
+# hashtag_data %>%
+#   group_by(date, hashtag) %>%
+#   summarise(mentions = n())
 
 posts <- read.csv(file.choose())
 
